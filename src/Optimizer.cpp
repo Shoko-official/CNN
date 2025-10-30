ff1e9f9a3440994cfb0519387a698134fb33a1c1
Shoko <a.breyton.pro@gmail.com>
2025-10-30 21:39:12 +0100

    Feat: ajout de SGB (update) : pour ConvLayer et FullyConnectedLayer

--- src/Optimizer.cpp
+++ src/Optimizer.cpp
@@ -4,9 +4,47 @@
 
 // Implémentation de SGD
 void SGD::update(ConvLayer& layer, float lr) {
+    // Mise à jour des noyaux
+    auto& kernels = layer.getKernels();
+    auto& kernelGradients = layer.getKernelGradients();
+    
+    for (size_t k = 0; k < kernels.size(); ++k) {
+        for (int z = 0; z < kernels[k].getDepth(); ++z) {
+            for (int y = 0; y < kernels[k].getHeight(); ++y) {
+                for (int x = 0; x < kernels[k].getWidth(); ++x) {
+                    kernels[k].at(z, y, x) -= lr * kernelGradients[k].at(z, y, x);
+                }
+            }
+        }
+    }
+    
+    // Mise à jour des biais
+    auto& biases = layer.getBiases();
+    auto& biasGradients = layer.getBiasGradients();
+    
+    for (size_t i = 0; i < biases.size(); ++i) {
+        biases[i] -= lr * biasGradients[i];
+    }
 }
 
 void SGD::update(FullyConnectedLayer& layer, float lr) {
+    // Mise à jour des poids
+    auto& weights = layer.getWeights();
+    auto& weightGradients = layer.getWeightGradients();
+    
+    for (int i = 0; i < layer.getOutputSize(); ++i) {
+        for (int j = 0; j < layer.getInputSize(); ++j) {
+            weights[i][j] -= lr * weightGradients[i][j];
+        }
+    }
+    
+    // Mise à jour des biais
+    auto& biases = layer.getBiases();
+    auto& biasGradients = layer.getBiasGradients();
+    
+    for (int i = 0; i < layer.getOutputSize(); ++i) {
+        biases[i] -= lr * biasGradients[i];
+    }
 }
 
 // Implémentation de Adam
