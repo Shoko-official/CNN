b4e4a456b8703fc73ddf8b3b63cf70e86b8fee4a
Shoko <a.breyton.pro@gmail.com>
2025-10-30 22:04:29 +0100

    feat : ajouté un début de la fonction d'optimiasation d'Adam (adaptive moment estimation

--- include/Optimizer.hpp
+++ include/Optimizer.hpp
@@ -3,6 +3,7 @@
 
 #include "ConvLayer.hpp"
 #include "FullyConnectedLayer.hpp"
+#include <vector>
 
 class Optimizer {
 public:
@@ -22,17 +23,28 @@ private:
     float beta1;
     float beta2;
     float epsilon;
-    float* m_weights; // Moment du premier ordre
-    float* v_weights; // Moment du second ordre
-    float* m_bias;
-    float* v_bias;
     int t; // Pas de temps
+    
+    // Stockage des moments pour ConvLayer
+    std::vector<std::vector<std::vector<float>>> m_conv_kernels; 
+    std::vector<std::vector<std::vector<float>>> v_conv_kernels; 
+    std::vector<float> m_conv_biases;     
+    std::vector<float> v_conv_biases;     
+    
+    // Stockage des moments pour FullyConnectedLayer
+    std::vector<std::vector<float>> m_fc_weights; 
+    std::vector<std::vector<float>> v_fc_weights; 
+    std::vector<float> m_fc_biases;              
+    std::vector<float> v_fc_biases;              
+    
+    // Indicateurs pour savoir si les moments ont été initialisés
+    bool conv_initialized;
+    bool fc_initialized;
 
 public:
     Adam(float beta1 = 0.9f, float beta2 = 0.999f, float epsilon = 1e-8f);
-    ~Adam();
     void update(ConvLayer& layer, float lr) override;
     void update(FullyConnectedLayer& layer, float lr) override;
 };
 
-#endif
+#endif // OPTIMIZER_HPP
--- src/Optimizer.cpp
+++ src/Optimizer.cpp
@@ -1,6 +1,7 @@
 #include "Optimizer.hpp"
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 
 // Implémentation de SGD
 void SGD::update(ConvLayer& layer, float lr) {
@@ -49,14 +50,80 @@ void SGD::update(FullyConnectedLayer& layer, float lr) {
 
 // Implémentation de Adam
 Adam::Adam(float beta1, float beta2, float epsilon) 
-    : beta1(beta1), beta2(beta2), epsilon(epsilon), t(0) {
-}
-
-Adam::~Adam() {
+    : beta1(beta1), beta2(beta2), epsilon(epsilon), t(0), conv_initialized(false), fc_initialized(false) {
 }
 
 void Adam::update(ConvLayer& layer, float lr) {
+    t++;
+    
+    auto& kernels = layer.getKernels();
+    auto& kernelGradients = layer.getKernelGradients();
+    auto& biases = layer.getBiases();
+    auto& biasGradients = layer.getBiasGradients();
+    
+    // Initialiser les moments si nécessaire
+    if (!conv_initialized) {
+        m_conv_kernels.resize(kernels.size());
+        v_conv_kernels.resize(kernels.size());
+        
+        for (size_t k = 0; k < kernels.size(); ++k) {
+            m_conv_kernels[k].resize(kernels[k].getDepth());
+            v_conv_kernels[k].resize(kernels[k].getDepth());
+            
+            for (int z = 0; z < kernels[k].getDepth(); ++z) {
+                m_conv_kernels[k][z].assign(kernels[k].getHeight() * kernels[k].getWidth(), 0.0f);
+                v_conv_kernels[k][z].assign(kernels[k].getHeight() * kernels[k].getWidth(), 0.0f);
+            }
+        }
+        
+        m_conv_biases.assign(biases.size(), 0.0f);
+        v_conv_biases.assign(biases.size(), 0.0f);
+        
+        conv_initialized = true;
+    }
+    
+    // Mise à jour des noyaux
+    for (size_t k = 0; k < kernels.size(); ++k) {
+        for (int z = 0; z < kernels[k].getDepth(); ++z) {
+            for (int y = 0; y < kernels[k].getHeight(); ++y) {
+                for (int x = 0; x < kernels[k].getWidth(); ++x) {
+                }
+            }
+        }
+    }
+    
+    // Mise à jour des biais
+    for (size_t i = 0; i < biases.size(); ++i) {
+
+    }
 }
 
 void Adam::update(FullyConnectedLayer& layer, float lr) {
+    t++;
+    
+    auto& weights = layer.getWeights();
+    auto& weightGradients = layer.getWeightGradients();
+    auto& biases = layer.getBiases();
+    auto& biasGradients = layer.getBiasGradients();
+    
+    // Initialiser les moments si nécessaire
+    if (!fc_initialized) {
+        m_fc_weights.resize(weights.size());
+        v_fc_weights.resize(weights.size());
+        
+        for (size_t i = 0; i < weights.size(); ++i) {
+            m_fc_weights[i].assign(weights[i].size(), 0.0f);
+            v_fc_weights[i].assign(weights[i].size(), 0.0f);
+        }
+        
+        m_fc_biases.assign(biases.size(), 0.0f);
+        v_fc_biases.assign(biases.size(), 0.0f);
+        
+        fc_initialized = true;
+    }
+    
+    // Ajouter : mise à jour des poids
 }
+
+
+// watching vidéo : https://www.youtube.com/watch?v=NE88eqLngkg
